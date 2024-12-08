17ee32198682d5441964ef268120a4f49da0717b
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2024-12-08 23:21:54 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -2,19 +2,85 @@ const express = require('express');
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
+
 const app = express();
+
+app.use(express.json());
 
 app.post("/signup", async (req,res) => {
 
-    const user = new User({
-        firstName : "Manish",
-        lastName: "Kumar",
-        emailId: "mkmonu68@gmail.com",
-        password: "Monu@123"
-    });
-    await user.save();
-    res.send("User added successfully");
+    const user = new User(req.body);
+
+    try{
+        await user.save();
+        res.send("User added successfully");
+    }catch(err){
+        res.status(400).send("Error saving the user" +err.message);
+    }
 });
+
+//Get user by emailId
+
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await User.find({emailId: userEmail});
+
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("Something went wrong !!!")
+    }
+})
+
+
+//Feed API - GET / feed - get all the user from database.
+
+app.get("/feed", async (req,res) => {
+    
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("Something went wrong !!!")
+    }
+    
+})
+
+//Delete a user from databse
+app.delete("/user", async (req,res) => {
+    const userId = req.body.userId;
+
+    try{
+
+        //const user = await User.findByIdAndDelete({_id:userId});
+        const user = await User.findByIdAndDelete(userId);
+
+        res.send("User deleted successfully");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong !!!")
+    }
+})
+
+//Update data of a user
+
+app.patch("/user", async (req,res) => {
+    const data = req.body;
+    const userId = req.body.userId;
+    try{
+        await User.findByIdAndUpdate({_id: userId}, data, {
+            runValidators: true
+        });
+        
+        res.send("User updataed Successfully");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong !!!")
+    }
+})
 
 connectDB().then(() => {
     console.log("Database Connect established");
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,50 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim : true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
         type: String
     },
     gender: {
+        type: String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+       
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.pngall.com/profile-png/download/51602/"
+    },
+    about: {
         type: String
+    },
+    skills: {
+        type: [String]
     }
-})
+},
+    {
+        timestamps: true
+    }
+);
 
 
 const User = mongoose.model("User",userSchema);
