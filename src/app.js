0efbaeb7afe41f2b16c56ce4d8f89634661cbd1e
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2024-12-09 00:49:43 +0530

    Api level sanitization

--- src/app.js
+++ src/app.js
@@ -67,10 +67,26 @@ app.delete("/user", async (req,res) => {
 
 //Update data of a user
 
-app.patch("/user", async (req,res) => {
+app.patch("/user/:userId", async (req,res) => {
+    const userId = req.params?.userId;
     const data = req.body;
-    const userId = req.body.userId;
+
     try{
+
+        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+
+        const isUpdateAllowed = Object.keys(data).every((k) => 
+            ALLOWED_UPDATES.includes(k)
+        );
+        if(!isUpdateAllowed)
+        {
+            throw new Error("Update not allowed");
+        }
+
+        if(data?.skills.length > 10)
+        {
+            throw new error("Skills cannot be more than 10");
+        }
         await User.findByIdAndUpdate({_id: userId}, data, {
             runValidators: true
         });
