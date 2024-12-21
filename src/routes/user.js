37a51b455f1b0c3ef576c09bc8c688496c1ba5dd
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2024-12-22 01:35:07 +0530

    feed - skip - limit

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const userRouter = express.Router();
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+
+const USER_SAFE_DATA = "firstName, lastName, photoUrl, age, about, skills";
 
 userRouter.get("/user/requests/received", userAuth , async (req,res) =>{
     try{
@@ -49,6 +52,47 @@ userRouter.get("/user/connections", userAuth , async (req,res)=>{
     }
 
 
+})
+
+userRouter.get("/feed", userAuth , async (req,res) =>{
+    try{
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page)
+        let limit = parseInt(req.query.limit)
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page-1)*limit;
+
+        console.log("Logged-in user ID:", loggedInUser._id);
+
+        // Find all connection requests (sent + received)
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        console.log("Connection Requests:", connectionRequests);
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) =>{
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString())
+        })
+
+        const users = await User.find({
+            $and: [
+                { _id: {$nin: Array.from(hideUsersFromFeed)}},
+                { _id: {$ne: loggedInUser._id}}
+            ],
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.send(users);
+
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
 })
 
 
